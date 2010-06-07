e2665e25bfab72265fd381cf6683226197b46fc3
Zainab Yasmeen <zainab.yasmeen@rofous.com>
2010-06-07 14:27:07 +0530

    Committing some changes in 1.java, 2.java and Helloworld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 public static void main(String args[]){
    System.out.println("HelloWorld");
+   This is for hello world typed on zainab's machine.
 // To - Do later 
 
 }
