124556c2d4f0e0f516d2409ff799ca3866cea74f
shobhitn <shobhit.nautiyal@rofous.com>
2010-06-25 12:47:18 +0530

    Adding the Hello.txt file and  made changes in  HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public static void main(String args[]){
 System.out.println("Nagarjuna");
 System.out.println("Rebase!!!!!!!");
 System.out.println("Rebase!!!!!!!!!!!!!!!!!!!!!!!!");
-// To - Do later 
+System.out.println("HelloWorld"); 
 
 }
 }
